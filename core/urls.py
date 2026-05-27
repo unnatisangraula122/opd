b0d8b73f95f8079be6e953d2243ac97fe3fe4d5b
unnati sangraula <unnatisangraula@gmail.com>
2026-05-27 21:58:07 +0545

    feat: add booking API with slot capacity enforcement

--- core/urls.py
+++ core/urls.py
@@ -3,4 +3,6 @@ from . import views
 
 urlpatterns = [
     path('health/', views.health_check, name='health'),
+    path('slots/', views.available_slots, name='available-slots'),
+    path('book/', views.book_token, name='book-token'),
 ]
